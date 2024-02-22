d637dee7e7d8791ebec0d0cb5b28b3fc9c3cb37d
Oriol <otornerc@gmail.com>
2024-02-22 13:59:21 +0100

    all models

---
+++ control_escolarRepo/control_escolarProgram/src/main/java/com/mycompany/main/Control_escolarProgram.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main;
+
+
+public class Control_escolarProgram {
+
+    public static void main(String[] args) {
+        
+    }
+}
