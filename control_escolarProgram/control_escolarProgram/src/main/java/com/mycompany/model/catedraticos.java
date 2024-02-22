7a83cc1fb2f2b2eaecd1877a7da41b6d287ffc7f
Mohamed Amin <elaminmohae@gmail.com>
2024-02-22 13:50:21 +0100

    creation of models bbdd

---
+++ control_escolarProgram/control_escolarProgram/src/main/java/com/mycompany/model/catedraticos.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.model;
+
+/**
+ *
+ * @author Mati
+ */
+public class catedraticos {
+    private int id;
+    private String nombre;
+
+    public catedraticos() {
+    }
+
+    public catedraticos(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    
+}
