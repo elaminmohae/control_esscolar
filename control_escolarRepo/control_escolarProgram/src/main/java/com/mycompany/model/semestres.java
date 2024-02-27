66c3156bfb8417baff4a13f1ac1f6b8801638d89
Mohamed Amin <elaminmohae@gmail.com>
2024-02-27 11:31:58 +0100

    Semestres update

--- control_escolarRepo/control_escolarProgram/src/main/java/com/mycompany/model/semestres.java
+++ control_escolarRepo/control_escolarProgram/src/main/java/com/mycompany/model/semestres.java
@@ -8,6 +8,49 @@ package com.mycompany.model;
  *
  * @author Mati
  */
-public class Semestres {
+public class semestres {
+    private int id;
+    private String nombre;
+    private int idCarreras;
+
+    
+    public semestres(int id, String nombre, int idCarreras) {
+        this.id = id;
+        this.nombre = nombre;
+        this.idCarreras = idCarreras;
+    }
+
+    public semestres() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getIdCarreras() {
+        return idCarreras;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setIdCarreras(int idCarreras) {
+        this.idCarreras = idCarreras;
+    }
+
+    @Override
+    public String toString() {
+        return "semestres{" + "id=" + id + ", nombre=" + nombre + ", idCarreras=" + idCarreras + '}';
+    }
+    
     
 }
